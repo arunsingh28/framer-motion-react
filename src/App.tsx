11ee4c54ffc8e94ac65512129c686543d2ef7576
Arun rPatap Singh <arunsingh28aug.as@gmail.com>
2022-05-14 21:38:07 +0530

    motion.div

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
 import React, { useEffect } from 'react';
+import { motion } from 'framer-motion'
+import images from './import/image'
 
 function App() {
 
-  useEffect(()=>{
+  useEffect(() => {
     document.title = "Slider"
   })
 
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <motion.div className='carousel'>
+        <motion.div className='inner-carousel'>
+          {
+            images.map(image =>{
+              return(
+                <img src={image} height={500}/>
+              )
+            })
+          }
+        </motion.div>
+      </motion.div>
     </div>
   );
 }
---
+++ src/import/image.ts
@@ -0,0 +1,7 @@
+import poster1 from '../assets/poster1.jpg'
+import poster2 from '../assets/poster2.jpg'
+import poster3 from '../assets/poster3.jpg'
+import poster4 from '../assets/poster4.jpg'
+import poster5 from '../assets/poster5.jpg'
+
+export default [ poster1, poster2, poster3, poster4, poster5]
