4216c0cf3c0df3f3d36a3f60de53a8f512fbfdf4
Arun rPatap Singh <arunsingh28aug.as@gmail.com>
2022-05-14 22:04:30 +0530

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,29 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { motion } from 'framer-motion'
 import images from './import/image'
+import { HTMLMotionComponents } from 'framer-motion/types/render/html/types';
 
 function App() {
 
   useEffect(() => {
     document.title = "Slider"
-  })
+
+  },[])
+
+  const [width, setWidth] = useState(0)
+
+  const carousel = useRef<HTMLDivElement | any>()
 
   return (
     <div className="App">
-      <motion.div className='carousel'>
-        <motion.div className='inner-carousel'>
+      <motion.div ref={carousel} className='carousel' >
+        <motion.div drag="x" dragConstraints={{ right: 0 , left: -1907}} className='inner-carousel'>
           {
-            images.map(image =>{
-              return(
-                <img src={image} height={500}/>
+            images.map(image => {
+              return (
+                <motion.div className='item'>
+                  <img src={image} height={500} alt={image} />
+                </motion.div>
               )
             })
           }
